f6ef62e50c969e86e3d8714a183f1b70bc81b843
Command <maoxs2@163.com>
2020-04-09 14:17:02 +0800

    fix TestPublicKey2Bytes

--- utils/key_test.go
+++ utils/key_test.go
@@ -9,8 +9,8 @@ import (
 
 func TestPublicKey2Bytes(t *testing.T) {
 	pk, _ := secp256k1.GeneratePrivateKey()
-	publicKey1 := pk.PubKey()
-	raw := PublicKey2Bytes(*publicKey1)
+	publicKey1 := *pk.PubKey()
+	raw := PublicKey2Bytes(publicKey1)
 	publicKey2 := Bytes2PublicKey(raw)
 	if !reflect.DeepEqual(publicKey1, publicKey2) {
 		t.Fail()
