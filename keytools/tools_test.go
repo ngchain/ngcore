6bcfb634c39466111e7fb4f48ac6a4192b48c087
Command <maoxs2@gmail.com>
2020-07-28 15:38:17 +0800

    add recover key test

--- keytools/tools_test.go
+++ keytools/tools_test.go
@@ -4,6 +4,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/mr-tron/base58"
 	"github.com/ngchain/ngcore/keytools"
 )
 
@@ -13,6 +14,13 @@ func TestKeyMgr_ReadLocalKey(t *testing.T) {
 	privKey2 := keytools.ReadLocalKey("ngtest.key", "test")
 
 	if !reflect.DeepEqual(privKey, privKey2) {
+		t.Log(privKey)
+		t.Log(privKey2)
+		t.Fail()
+	}
+
+	pk := keytools.RecoverLocalKey("ngtest.key", "test", base58.FastBase58Encoding(privKey.Serialize()))
+	if !reflect.DeepEqual(pk, privKey) {
 		t.Log(privKey)
 		t.Log(privKey2)
 		t.Fail()
