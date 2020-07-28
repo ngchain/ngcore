6a17a9331417dee5c4a84533954b4ae895e3c5ba
Command <maoxs2@gmail.com>
2020-07-28 17:36:07 +0800

    bump version

--- cmd/ngcore/defaults.go
+++ cmd/ngcore/defaults.go
@@ -5,7 +5,7 @@ const (
 	usage       = "Brand-new golang daemon implement of Ngin Network Node"
 	description = "NGIN is a radically updating brand-new blockchain network, " +
 		"which is not a fork of ethereum or any other chain."
-	version = "v0.0.16"
+	version = "v0.0.17"
 
 	// flag values
 	defaultTCPP2PPort   = 52520
