41fde24515718b93eb2f7cd0b2b89626bd926ece
Command <maoxs2@gmail.com>
2020-08-21 20:23:51 +0800

    fix miner unit test

--- miner/miner_test.go
+++ miner/miner_test.go
@@ -3,11 +3,14 @@ package miner_test
 import (
 	miner "github.com/ngchain/ngcore/miner"
 	"github.com/ngchain/ngcore/ngtypes"
+	"math/big"
 	"testing"
 )
 
 func TestPoWMiner(t *testing.T) {
 	block := ngtypes.GetGenesisBlock()
+
+	block.Difficulty = big.NewInt(100).Bytes() // lower for avoid timeout
 
 	ch := make(chan *ngtypes.Block)
 	m := miner.NewMiner(2, ch)
